8719a30b5804a4acfd7e152aff4f0ee78e221a79
AleksandrPalenko <palenko.as@mail.ru>
2021-04-25 11:08:42 +0300

    puzzle

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,10 +2,10 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 1 && (checkVertical(board, 1) || checkAndHorizontal(board, 1))) {
-                rsl = false;
+                rsl = true;
                 break;
             }
         }
